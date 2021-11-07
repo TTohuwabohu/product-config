e2d4331136048585c22074718ac16b67231d63de
Cedric <cmartens@uni-potsdam.de>
2021-11-07 22:09:45 +0100

    cleanup

--- product-knowledge/bike/clingoInterpreter.py
+++
@@ -1,8 +0,0 @@
-from io import StringIO
-
-
-def clingoInterpreter(clingoString : str):
-	clingoString = str(clingoString)
-	clingoString = clingoString.replace(r'\r\n', '\r\n')
-	print(clingoString)
-		
